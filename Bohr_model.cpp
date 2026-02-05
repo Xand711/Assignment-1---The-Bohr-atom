4388f7ee4bfb4b18a16957e991f4602879709767
Charles Bamsey <xandbamsey@gmail.com>
2026-02-05 20:15:13 +0000

    Commented and brought energy levels into same input.

--- Bohr_model.cpp
+++ Bohr_model.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
+    // Initialise variables
     int Z;
     int n_initial;
     int n_final;
+    // Ask for inputs
     cout << "Enter the atomic number (Z) of the atom." << endl;
     cin >> Z;
-    cout << "Enter the principle quantum number of the intial energy level."<< endl;
-    cin >> n_initial;
-    cout << "Enter the principle quantum number of the final energy level."<< endl;
-    cin >> n_final;
+    cout << "Enter the principle quantum numbers (n) of the intial and final energy levels."<< endl;
+    cin >> n_initial >> n_final;
     return 0;
 }
