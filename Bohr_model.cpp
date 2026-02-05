c7ba031b9a008af5b35b65393d33c5e836a0487c
Charles Bamsey <xandbamsey@gmail.com>
2026-02-05 19:54:01 +0000

    Code  takes inputs for all three parameters.

---
+++ Bohr_model.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int Z;
+    int n_initial;
+    int n_final;
+    cout << "Enter the atomic number (Z) of the atom." << endl;
+    cin >> Z;
+    cout << "Enter the principle quantum number of the intial energy level."<< endl;
+    cin >> n_initial;
+    cout << "Enter the principle quantum number of the final energy level."<< endl;
+    cin >> n_final;
+    return 0;
+}
