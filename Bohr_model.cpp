47c3419c093a457e4c5fab1d117db3612e7534d9
Charles Bamsey <xandbamsey@gmail.com>
2026-02-06 18:16:20 +0000

    Moved calculation of energy into separate function. Added repition condition and input. Added <= 0 bad input conditions. Added correct Joules unit conversion value.

--- Bohr_model.cpp
+++ Bohr_model.cpp
@@ -2,6 +2,12 @@
 #include <cmath>
 #include <limits>
 using namespace std;
+
+double calc_energy(int Z, int n_i, int n_f, double conv) {
+    //Calculates photon energy
+    return fabs(conv * 13.6 * pow(Z,2) * (1/(pow(n_f,2)) - 1/(pow(n_i,2)))) ;
+}
+
 int main() {
     // Initialise variables
     int Z;
@@ -10,80 +16,94 @@ int main() {
     char unit;
     double conv;
     bool eVs;
-    // Ask for inputs
-    cout << "Enter the atomic number (Z) of the atom." << endl;
-    cin >> Z;
-    while(true) {
-        // Checks for invalid input including floats
-        if (cin.fail() || cin.peek() == '.') {
-            // Clears invalid input and asks for new input
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(),'\n');
-            cout << "Invalid input, please enter integer value." << endl;
-            cin >> Z;
-        }
-        else if(Z == 0){
-            // Ends program if Z = 0
-            cout << "Invalid Z value.";
-            return 0;
-        }
-        else{
-            // Breaks loop if input is valid
-            break;
-        }
-    }
-    cout << "Enter the principle quantum numbers (n) of the intial and final energy levels."<< endl;
-    cin >> n_initial >> n_final;
+    char repeat;
+    while(true){
+        // Ask for input parameters
+        cout << "Enter the atomic number (Z) of the atom." << endl;
         while(true) {
-        // Checks for invalid inputs including floats
-        if (cin.fail() || cin.peek() == '.') {
-            // Clears invalid input and asks for new input
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(),'\n');
-            // Asks for new valid inputs
-            cout << "Invalid input, please enter integer values." << endl;
+            cin >> Z;
+            // Checks for invalid input including floats
+            if (cin.fail() || cin.peek() == '.') {
+                // Clears invalid input and asks for new input
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(),'\n');
+                cout << "Invalid input, please enter integer value." << endl;
+            }
+            else if(Z <= 0){
+                // Repeats loop if Z =< 0
+                cout << "Invalid Z value, please enter valid integer."  << endl;
+            }
+            else{
+            // Breaks loop if input is valid
+                break;
+            }
+        }
+        cout << "Enter the principle quantum numbers (n) of the intial and final energy levels."<< endl;
+         while(true) {
             cin >> n_initial >> n_final;
+            // Checks for invalid inputs including floats
+            if (cin.fail() || cin.peek() == '.') {
+                // Clears invalid input and asks for new input
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(),'\n');
+                // Asks for new valid inputs
+                cout << "Invalid input, please enter integer values." << endl;
+            }
+            else if( n_initial < n_final){
+             // Checks for n_initial < n_final
+             cout << "n_initial < n_final. Invalid input, please try again." << endl;
+            }
+            else if(n_initial <= 0 || n_final <= 0){
+                cout << "Invalid value. Please try again" << endl;
+            }
+            else{
+             // Breaks while loop if input is valid
+                break;
+            }
         }
-        else if( n_initial < n_final){
-            // Checks for n_initial < n_final
-            cout << "n_initial < n_final. Invalid input, please try again." << endl;
-            cin >> n_initial >> n_final;
-        }
-        else{
-            // Breaks while loop if input is valid
-            break;
-        }
-    }
-    cout << "Would you like energy in units of Joules or electronvolts? [J/E]" << endl;
-    cin >> unit;
-    // Checks value of unit for desired energy unit. Sets conversion value based on input.
-    while (true) {
-        if (toupper(unit) == 'E'){
-            conv = 1;
-            eVs = true;
-           break;
-        }
-        else if (toupper(unit) == 'J'){
-            // placeholder conversion value
-            conv = 2.5;
-            eVs = false;
-            break;
-        }
-        else{
-            cout << "Your input was invalid. Please input J or E for desired unit." << endl;
+        cout << "Would you like energy in units of Joules or electronvolts? [J/E]" << endl;
+        // Checks value of unit for desired energy unit. Sets conversion value based on input.
+        while (true) {
             cin >> unit;
+            if (toupper(unit) == 'E'){
+                conv = 1;
+                eVs = true;
+            break;
+            }
+            else if (toupper(unit) == 'J'){
+                // placeholder conversion value
+                conv = 1.602176e-19;
+                eVs = false;
+                break;
+            }
+            else{
+                cout << "Your input was invalid. Please input J or E for desired unit." << endl;
+            }
+        }
+        // Calculate and display photon energy
+        double Energy = calc_energy(Z, n_initial, n_final, conv);
+        cout << "The photon energy released by an electron transition from state n = "
+         << n_initial << " to n = " << n_final << " is " << Energy;
+            // Prints correct unit
+             if(eVs == true){
+              cout << " eV" << endl;
+            } 
+            else{
+             cout << " J" << endl;
+            }
+        // Asks if user wants to do another calculation
+        cout << "Would you like to perform another calculation? [Y/N]" << endl;
+        while(true){
+            cin >> repeat;
+            if (toupper(repeat) == 'Y'){
+                break;
+            }
+            else if (toupper(repeat) == 'N'){
+                return 0;
+            }
+            else{
+                cout << "Invalid input. Please try again." << endl;
         }
     }
-    // Calculate and display photon energy
-    double Energy = fabs(conv * 13.6 * pow(Z,2) * (1/(pow(n_final,2)) - 1/(pow(n_initial,2))));
-    cout << "The photon energy released by an electron transition from state n = " << n_initial 
-        << " to n = " << n_final << " is " << Energy;
-        // Prints correct unit
-         if(eVs == true){
-            cout << " eV"<< endl;
-        } 
-        else{
-            cout << " J" << endl;
-        }
-    return 0;
+    }
 }
