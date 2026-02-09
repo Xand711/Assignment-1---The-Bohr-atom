b292b42a97a8dd998f07b774f828a4a8f445e33b
Charles Bamsey <xandbamsey@gmail.com>
2026-02-09 17:32:29 +0000

    Added whitespace and comments

--- Bohr_model.cpp
+++ Bohr_model.cpp
@@ -15,29 +15,36 @@ int main() {
     int n_final;
     char unit;
     double conv;
-    bool eVs;
+    bool evs_unit;
     char repeat;
+
+    // Code repeats until user stops the process 
     while(true){
         // Ask for input parameters
         cout << "Enter the atomic number (Z) of the atom." << endl;
+
+        // While loop takes for input and checks validity, only breaks loop upon valid input
         while(true) {
             cin >> Z;
-            // Checks for invalid input including floats
+            // Checks for invalid data types including floats
             if (cin.fail() || cin.peek() == '.') {
                 // Clears invalid input and asks for new input
                 cin.clear();
                 cin.ignore(numeric_limits<streamsize>::max(),'\n');
                 cout << "Invalid input, please enter integer value." << endl;
             }
+
             else if(Z <= 0){
-                // Repeats loop if Z =< 0
+            // Asks for new input if Z =< 0
                 cout << "Invalid Z value, please enter valid integer."  << endl;
             }
+
             else{
             // Breaks loop if input is valid
                 break;
             }
         }
+
         cout << "Enter the principle quantum numbers (n) of the intial and final energy levels."<< endl;
          while(true) {
             cin >> n_initial >> n_final;
@@ -49,33 +56,38 @@ int main() {
                 // Asks for new valid inputs
                 cout << "Invalid input, please enter integer values." << endl;
             }
+
             else if( n_initial < n_final){
              // Checks for n_initial < n_final
              cout << "n_initial < n_final. Invalid input, please try again." << endl;
             }
+
             else if(n_initial <= 0 || n_final <= 0){
                 cout << "Invalid value. Please try again" << endl;
             }
+
             else{
              // Breaks while loop if input is valid
                 break;
             }
         }
         cout << "Would you like energy in units of Joules or electronvolts? [J/E]" << endl;
-        // Checks value of unit for desired energy unit. Sets conversion value based on input.
+        // Checks input for desired energy unit. Sets conversion value based on input.
+        // Also determines which unit is displayed at in the answer output.
         while (true) {
             cin >> unit;
             if (toupper(unit) == 'E'){
                 conv = 1;
-                eVs = true;
+                evs_unit = true;
             break;
             }
+
             else if (toupper(unit) == 'J'){
-                // placeholder conversion value
                 conv = 1.602176e-19;
-                eVs = false;
+                evs_unit = false;
                 break;
             }
+
             else{
                 cout << "Your input was invalid. Please input J or E for desired unit." << endl;
             }
@@ -84,13 +96,15 @@ int main() {
         double Energy = calc_energy(Z, n_initial, n_final, conv);
         cout << "The photon energy released by an electron transition from state n = "
          << n_initial << " to n = " << n_final << " is " << Energy;
-            // Prints correct unit
-             if(eVs == true){
+         // Prints correct unit
+             if(evs_unit == true){
               cout << " eV" << endl;
             } 
+
             else{
              cout << " J" << endl;
             }
+
         // Asks if user wants to do another calculation
         cout << "Would you like to perform another calculation? [Y/N]" << endl;
         while(true){
@@ -98,12 +112,14 @@ int main() {
             if (toupper(repeat) == 'Y'){
                 break;
             }
+
             else if (toupper(repeat) == 'N'){
                 return 0;
             }
+
             else{
                 cout << "Invalid input. Please try again." << endl;
+            }
         }
-    }
     }
 }
